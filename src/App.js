b5ac9edc2bdebb59929de2a117f0de2e3cb9c40c
mojoe15 <eliranger15@gmail.com>
2021-11-05 09:55:39 -0400

    added content to about and skills

--- src/App.js
+++ src/App.js
@@ -11,27 +11,27 @@ import Portfolio from "./Components/Portfolio";
 import "./App.css";
 
 const App = () => {
-  const [resumeData, setResumeData] = useState({});
+    const [resumeData, setResumeData] = useState({});
 
-  useEffect(() => {
-    fetch("/resumeData.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setResumeData(data);
-      });
-  }, []);
+    useEffect(() => {
+        fetch("/resumeData.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setResumeData(data);
+            });
+    }, []);
 
-  return (
-    <div className="App">
-      <Header data={resumeData.main} />
-      <About data={resumeData.main} />
-      <Resume data={resumeData.resume} />
-      <Portfolio data={resumeData.portfolio} />
-      <Testimonials data={resumeData.testimonials} />
-      <Contact data={resumeData.main} />
-      <Footer data={resumeData.main} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header data={resumeData.main} />
+            <About data={resumeData.main} />
+            <Resume data={resumeData.resume} />
+            <Portfolio data={resumeData.portfolio} />
+            <Testimonials data={resumeData.testimonials} />
+            <Contact data={resumeData.main} />
+            <Footer data={resumeData.main} />
+        </div>
+    );
 };
 
 export default App;
