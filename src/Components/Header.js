ea39ba59a2b68d4229741246f53298382b1af16a
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2021-12-05 10:46:57 -0500

    changing landing page message

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,86 +2,97 @@ import React from "react";
 import TypeWriter from "react-typewriter";
 
 const Header = ({ data }) => {
-  if (data) {
-    var name = data.name;
-    var occupation = data.occupation;
-    var description = data.description;
-    var city = data.address.city;
-    var networks = data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
-  }
+	if (data) {
+		var firstName = data.firstName;
+		var occupation = data.occupation;
+		var description = data.description;
+		var city = data.address.city;
+		var networks = data.social.map(function (network) {
+			return (
+				<li key={network.name}>
+					<a href={network.url}>
+						<i className={network.className}></i>
+					</a>
+				</li>
+			);
+		});
+	}
 
-  return (
-    <header id="home">
-      <nav id="nav-wrap">
-        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-          Show navigation
-        </a>
-        <a className="mobile-btn" href="#home" title="Hide navigation">
-          Hide navigation
-        </a>
+	return (
+		<header id="home">
+			<nav id="nav-wrap">
+				<a
+					className="mobile-btn"
+					href="#nav-wrap"
+					title="Show navigation"
+				>
+					Show navigation
+				</a>
+				<a className="mobile-btn" href="#home" title="Hide navigation">
+					Hide navigation
+				</a>
+				<ul id="nav" className="nav">
+					{/* <li>
+						<a id="header-morgan" href="#">
+							Morgan Johnson
+						</a>
+					</li> */}
+					<li className="current">
+						<a className="smoothscroll" href="#home">
+							Home
+						</a>
+					</li>
+					<li>
+						<a className="smoothscroll" href="#about">
+							About
+						</a>
+					</li>
+					<li>
+						<a className="smoothscroll" href="#resume">
+							Resume
+						</a>
+					</li>
+					<li>
+						<a className="smoothscroll" href="#portfolio">
+							Works
+						</a>
+					</li>
+					<li>
+						<a className="smoothscroll" href="#testimonials">
+							Testimonials
+						</a>
+					</li>
+					<li>
+						<a className="smoothscroll" href="#contact">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</nav>
 
-        <ul id="nav" className="nav">
-          <li className="current">
-            <a className="smoothscroll" href="#home">
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#about">
-              About
-            </a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#resume">
-              Resume
-            </a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#portfolio">
-              Works
-            </a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#testimonials">
-              Testimonials
-            </a>
-          </li>
-          <li>
-            <a className="smoothscroll" href="#contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
+			<div className="row banner">
+				<div className="banner-text">
+					<h1 className="responsive-headline">
+						<TypeWriter typing={0.5}>
+							{firstName ? `Hi, I'm ${firstName}!` : null}
+						</TypeWriter>
+					</h1>
+					<h3>
+						Based in {city}. <span>{occupation}</span>.{" "}
+						{description}.
+					</h3>
+					<hr />
+					<ul className="social">{networks}</ul>
+				</div>
+			</div>
 
-      <div className="row banner">
-        <div className="banner-text">
-          <h1 className="responsive-headline">
-            <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
-          </h1>
-          <h3>
-            Based in {city}. <span>{occupation}</span>. {description}.
-          </h3>
-          <hr />
-          <ul className="social">{networks}</ul>
-        </div>
-      </div>
-
-      <p className="scrolldown">
-        <a className="smoothscroll" href="#about">
-          <i className="icon-down-circle"></i>
-        </a>
-      </p>
-    </header>
-  );
+			<p className="scrolldown">
+				<a className="smoothscroll" href="#about">
+					<i className="icon-down-circle"></i>
+				</a>
+			</p>
+		</header>
+	);
 };
 
 export default Header;
