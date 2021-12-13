7cd611d91428bbd84b28f9cd682df70c2ed09748
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2021-12-13 06:03:05 -0500

    misspelled word

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -34,7 +34,7 @@ const Portfolio = ({ data }) => {
 		<section id="portfolio">
 			<div className="row">
 				<div className="twelve columns collapsed">
-					<h1>Check Out Some of My Works.</h1>
+					<h1>Check Out Some of My Work!</h1>
 
 					<div
 						id="portfolio-wrapper"
